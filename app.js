2c0e01484babc92a8ff1dd40cc53063ba4d42a4c
arakidrecome <araki@j-o-s.net>
2019-06-28 14:39:35 +0900

    デモ勤怠管理システム
    デモ機コミットテスト

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var menberlistRouter = require('./routes/menberlist');
+var menbereditRouter = require('./routes/menberedit');
 
 var app = express();
 
@@ -19,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', loginRouter);
+app.use('/', menberlistRouter);
+app.use('/', menbereditRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
